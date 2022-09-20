381c54e5f4a9dcef7b811d0a49d277a6ea7dfa0c
Houdja <houdja-ahamadi976@hotmail.com>
2022-09-20 08:01:38 +0400

    function create task & recupération des taches

--- js/app.js
+++ js/app.js
@@ -1,7 +1,51 @@
 const form = document.querySelector('.add__tasks');
+const todo = document.querySelector('.todo div');
 
+
+//CREATE TASK
+
+function createTask(t){
+
+    let div = document.createElement('div')
+    div.classList.add('task')
+    div.innerHTML = `
+        <p>${t.task}</p>
+        <div class="contain__btn">
+            <span class="material-icons">
+                trip_origin
+            </span>
+            <span class="material-icons">
+                edit
+            </span>
+        </div>`
+
+        return div;
+}
 
 //RECUPERER LES TACHES
+
+let tasks = function(){
+    fetch('php/get_task.php')
+    .then(response => {
+        if(response.ok){
+            return response.json();
+        }
+    })
+    .then(data => {
+        if(data[0]){
+            data.forEach(task => {
+                todo.appendChild(createTask(task));
+            })
+        }else{
+            todo.innerHTML = 'Aucun tâche...'
+        }
+    })
+    .catch(e => {
+        console.log("Erreur" + e);
+    })
+}
+
+tasks();
 
 // AJOUTER UNE TACHE
 
@@ -22,6 +66,8 @@ form.addEventListener('submit', function(e){
         if(data.success){
             alert(data.msg);
             form.querySelector('input').value = '';
+            todo.innerHTML = '';
+            tasks();
         }else{
             alert(data.msg);
         }
